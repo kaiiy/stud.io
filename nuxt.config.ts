d7efbd268d083a2786770c989661fbf5fa5eefcf
kaiiy <kaiiy@kaiix.tokyo>
2022-05-12 22:00:52 +0900

    chore: update ts config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: [
-        'normalize.css',
-        '@/assets/css/component.css'
-    ],
-    modules: [
-        'nuxt-windicss',
-    ],
-})
+  css: ["normalize.css", "@/assets/css/component.css"],
+  modules: ["nuxt-windicss"],
+  typescript: {
+    strict: true,
+  },
+});
