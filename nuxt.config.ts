a84ee250b05575f607d52c96d7cca72006c03121
kaiiy <kaiiy@kaiix.tokyo>
2022-05-14 02:07:26 +0900

    chore: update nuxt core

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["normalize.css", "@/assets/css/component.css"],
-  modules: ["nuxt-windicss"],
+  modules: ["nuxt-windicss", "@nuxtjs/pwa"],
   typescript: {
     strict: true,
   },
