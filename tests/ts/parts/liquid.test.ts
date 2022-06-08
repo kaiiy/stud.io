03ef7903e8bdc0524357f70eda54ab66de565d55
kaiiy <kaiiy@kaiix.tokyo>
2022-06-09 00:51:28 +0900

    refactor: rename dict

--- tests/ts/parts/liquid.test.ts
+++ tests/ts/parts/liquid.test.ts
@@ -1,6 +1,6 @@
 import { correctLiquid4Drawing } from "../../../assets/ts/parts/liquid";
 
-test("Liquid overflows", () => {
+test("liquid overflows", () => {
   const VAL_GREATER_THAN_ONE = (Math.random() + 1) * 1000;
   expect(correctLiquid4Drawing(VAL_GREATER_THAN_ONE)).toBe(1);
 });
