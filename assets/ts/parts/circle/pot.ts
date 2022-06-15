9e554ee577004ce1965a1e7596c714cec7463999
kaiiy <kaiiy@kaiix.tokyo>
2022-06-15 18:50:24 +0900

    fix: pot range err

--- assets/ts/parts/circle/pot.ts
+++ assets/ts/parts/circle/pot.ts
@@ -1,8 +1,7 @@
 const adjustPotRate = (potRate: number) => {
-  if (potRate < 0 || potRate > 1) throw RangeError();
-
   const BOTTOM_CORRECTION_RATIO = 0.03;
   const TOP_CORRECTION_RATIO = 0.06;
+
   if (potRate < BOTTOM_CORRECTION_RATIO) return 0;
   if (potRate > 1 - TOP_CORRECTION_RATIO) return 1;
   return potRate;
