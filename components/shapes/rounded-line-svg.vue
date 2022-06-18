6c23cf2b84d838b6ebd39fbc98d9e9bb6babdc0e
kaiiy <kaiiy@kaiix.tokyo>
2022-06-18 16:28:25 +0900

    feat: add rounded-line-svg

---
+++ components/shapes/rounded-line-svg.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  heightPx: number,
+  widthPx: number,
+  color: string,
+}>();
+
+const viewBoxStr = computed(() => `0, 0, ${props.widthPx}, ${props.heightPx}`)
+const cornerRadius = computed(() => Math.min(props.heightPx / 2, props.widthPx / 2))
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="widthPx" :height="heightPx" :viewBox="viewBoxStr">
+    <rect :width="widthPx" :height="heightPx" :fill="color" :rx="cornerRadius" :ry="cornerRadius"></rect>
+  </svg>
+</template>
