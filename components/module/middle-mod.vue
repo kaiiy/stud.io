50568bb872d3313a4730da9b2de1ec44242826a1
kaiiy <81578246+kaiiy@users.noreply.github.com>
2022-07-22 00:26:34 +0900

    wip: noodles & liquid line

--- components/module/middle-mod.vue
+++ components/module/middle-mod.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { getMiddleModSize, getNoodleSize } from "assets/ts/parts/get-size"
+import { getMiddleModSize } from "assets/ts/parts/get-size"
 import { heightPx, widthPx, leftPx, topPx } from "assets/ts/style/to-px"
 import { getLiquidSize, getLiquidTop } from "assets/ts/parts/liquid"
 import { COLOR } from "@/assets/ts/style/color"
-import NoodleSvg from "../shapes/noodle-svg.vue"
+import Noodles from "./middle/noodles.vue"
+import Container from "./middle/container.vue"
 
 const props = defineProps<{
     baseSize: number,
@@ -16,10 +17,6 @@ const {
     innerHeight: modInnerHeight, innerWidth: modInnerWidth,
     innerTop: modInnerTop, innerLeft: modInnerLeft
 } = getMiddleModSize(props.baseSize)
-
-const {
-    height: noodleHeight, width: NoodleWidth
-} = getNoodleSize(props.baseSize)
 
 // liquid 
 const liquidHeight = computed<number>(() => {
@@ -40,21 +37,20 @@ const liquidTop = computed<number>(() => {
             ...topPx(liquidTop), ...leftPx(modInnerLeft)
         }"></div>
 
-        <NoodleSvg class="absolute" :style="{
-            top: '50px', ...heightPx(noodleHeight), ...widthPx(NoodleWidth)
+        <!-- container  -->
+        <Container class="comp-default" :style="{
+            ...heightPx(modHeight), ...widthPx(modWidth)
         }" :color="COLOR.LIGHT_PURPLE" />
 
-        <!-- container  -->
-        <svg class="comp-default" :style="{
-            ...heightPx(modHeight), ...widthPx(modWidth)
-        }" version="1.1" viewBox="0 0 55.563 108.48" xmlns="http://www.w3.org/2000/svg">
-            <g transform="translate(-87.312 -5.2917)">
-                <path
-                    d="m89.334 5.2917c-1.1196 0-2.0211 0.85955-2.0211 1.9271v104.63c0 1.0675 0.90148 1.9271 2.0211 1.9271h51.52c1.1196 0 2.0211-0.85955 2.0211-1.9271v-104.63c0-1.0675-0.90149-1.9271-2.0211-1.9271zm25.76 8.8297c5.1303 0 9.2604 3.938 9.2604 8.8297v73.16c0 4.8917-4.1302 8.8297-9.2604 8.8297-5.1303 0-9.2604-3.938-9.2604-8.8297v-73.16c0-4.8917 4.1301-8.8297 9.2604-8.8297z"
-                    :fill="COLOR.LIGHT_PURPLE" stroke-linecap="round" stroke-width="1.2878" />
-            </g>
-        </svg>
+        <Noodles :base-size="baseSize" />
 
+        <!-- liquid line (cup)  -->
+        <svg :style="{
+            strokeWidth: '4px',
+            top: '130px'
+        }" class="absolute" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0" y1="0" x2="100" y2="0" :stroke="COLOR.DARK_PURPLE" />
+        </svg>
     </div>
 </template>
 
---
+++ components/module/middle/noodles.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { getNoodleSize } from "assets/ts/parts/get-size"
+import { heightPx, widthPx } from "assets/ts/style/to-px"
+import { baseLeftPx, baseTopPx } from "@/assets/ts/parts/middle/noodle"
+import { rotateOnly } from "~~/assets/ts/style/transform"
+import { COLOR } from "@/assets/ts/style/color"
+import Noodle from "./noodle.vue"
+
+const props = defineProps<{
+  baseSize: number,
+}>();
+
+const {
+  height: noodleHeight, width: NoodleWidth
+} = getNoodleSize(props.baseSize)
+
+const noodleColor: string = COLOR.DARK_PURPLE
+</script>
+
+<template>
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(50), ...baseLeftPx(0.1, baseSize), ...baseTopPx(1, baseSize)
+  }" :color="noodleColor" />
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(4), ...baseLeftPx(0.5, baseSize), ...baseTopPx(0.4, baseSize)
+  }" :color="noodleColor" />
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(0), ...baseLeftPx(0.2, baseSize), ...baseTopPx(0.8, baseSize)
+  }" :color="noodleColor" />
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(80), ...baseLeftPx(0.1, baseSize), ...baseTopPx(1, baseSize)
+  }" :color="noodleColor" />
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(20), ...baseLeftPx(0.5, baseSize), ...baseTopPx(0.4, baseSize)
+  }" :color="noodleColor" />
+  <Noodle class="absolute" :style="{
+    ...heightPx(noodleHeight), ...widthPx(NoodleWidth), ...rotateOnly(10), ...baseLeftPx(0.2, baseSize), ...baseTopPx(0.8, baseSize)
+  }" :color="noodleColor" />
+</template>
