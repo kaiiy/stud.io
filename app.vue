e822be227c5390e7fb1937ce12390164069b808a
k <81578246+kaiiy@users.noreply.github.com>
2025-03-03 21:18:23 +0900

    top page

--- app.vue
+++ app.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
 import Game from "@/components/main/game.vue"
+import SwitchBase from "@/components/module/switch/base.vue"
+import { heightPx, topPx, leftPx, widthPx } from "assets/ts/style/to-px"
+import { COLOR } from "@/assets/ts/style/color"
+import BtnBase from "@/components/module/next/btn-base.vue"
+
+const hasStarted = ref(false)
+const startGame = () => {
+  hasStarted.value = true
+}
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
+  <div v-show="!hasStarted" class="flex flex-col items-center justify-center h-screen">
     <div class="text-left">
-      <p class="font-noto-mono text-9xl">stud.io</p>
-      <div class="font-noto-jp text-2xl flex items-center">
-        <Icon name="prime:angle-double-right" size="48" />
-        <p>Created by kaiiy</p>
+      <div class="flex items-end">
+        <BtnBase @click="startGame" class="cursor-pointer hover-bright" :style="{
+          ...heightPx(176), ...widthPx(176)
+        }" :base-color="COLOR.DARK_PURPLE" :inner-color="COLOR.LIGHT_PURPLE" />
+        <div class="ml-8">
+          <p class="font-noto-mono text-9xl">
+            stud.io
+          </p>
+          <div class="font-noto-jp text-2xl flex items-center">
+            <Icon name="prime:angle-double-right" size="48" />
+            <p>Created by kaiiy</p>
+          </div>
+        </div>
       </div>
     </div>
-    <button
-      class="mt-12 px-6 py-3 text-2xl font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 transition duration-300">
-      Game Start
-    </button>
   </div>
 
-
-
-
-  <!-- <Game /> -->
+  <Game v-show="hasStarted" />
 </template>
 
 <style scoped>
@@ -30,5 +41,14 @@ import Game from "@/components/main/game.vue"
 
 .font-noto-jp {
   font-family: 'Noto Sans JP', sans-serif;
+}
+
+
+.hover-bright {
+  transition: filter 0.3s ease;
+}
+
+.hover-bright:hover {
+  filter: brightness(1.1);
 }
 </style>
